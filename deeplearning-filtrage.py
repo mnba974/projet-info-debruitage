eea1ccb4efc99b6326f3310080788978b6946bd3
Abdellah-Mines <112401785+Abdellah-Mines@users.noreply.github.com>
2023-07-06 10:37:47 +0200

    Add files via upload

--- deeplearning-filtrage.py
+++ deeplearning-filtrage.py
@@ -57,64 +57,20 @@ def display(array1, array2):
 
     plt.show()
 
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
+# Since we only need images from the dataset to encode and decode, we
+# won't use the labels.
+(train_data, _), (test_data, _) = mnist.load_data()
 
-from tensorflow.keras import layers
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Model
+# Normalize and reshape the data
+train_data = preprocess(train_data)
+test_data = preprocess(test_data)
 
+# Create a copy of the data with added noise
+noisy_train_data = noise(train_data)
+noisy_test_data = noise(test_data)
 
-def preprocess(array):
-    """
-    Normalizes the supplied array and reshapes it into the appropriate format.
-    """
-
-    array = array.astype("float32") / 255.0
-    array = np.reshape(array, (len(array), 28, 28, 1))
-    return array
-
-
-def noise(array):
-    """
-    Adds random noise to each image in the supplied array.
-    """
-
-    noise_factor = 0.4
-    noisy_array = array + noise_factor * np.random.normal(
-        loc=0.0, scale=1.0, size=array.shape
-    )
-
-    return np.clip(noisy_array, 0.0, 1.0)
-
-
-def display(array1, array2):
-    """
-    Displays ten random images from each one of the supplied arrays.
-    """
-
-    n = 10
-
-    indices = np.random.randint(len(array1), size=n)
-    images1 = array1[indices, :]
-    images2 = array2[indices, :]
-
-    plt.figure(figsize=(20, 4))
-    for i, (image1, image2) in enumerate(zip(images1, images2)):
-        ax = plt.subplot(2, n, i + 1)
-        plt.imshow(image1.reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        ax = plt.subplot(2, n, i + 1 + n)
-        plt.imshow(image2.reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-    plt.show()
+# Display the train data and a version of it with added noise
+display(train_data, noisy_train_data)
 
 input = layers.Input(shape=(28, 28, 1))
 
